4833abed1742bf372b71d56c3708d489d5ab5964
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2024-11-17 12:39:22 +0900

    Update state-prohibitAttacks.js

--- ステート_カスタム/state-prohibitAttacks.js
+++ ステート_カスタム/state-prohibitAttacks.js
@@ -3,7 +3,7 @@
 state-prohibitAttacks.js
 
 ■SRPG Studio対応バージョン
-ver.1.3031
+ver.1.304
 
 ■プラグインの概要
 「攻撃を禁止する」ステートを実装します。
@@ -14,14 +14,16 @@ ver.1.3031
 ■使用方法
 1.このプラグインをpluginフォルダに入れる
 2．任意のステートを作成する
-3．2で作成したステートのidを本プラグイン内(45行目付近)の STATE_ID に設定する
-
-4．AIのステート評価値を変更したい場合、ステートのカスタムパラメータに任意の値を設定する
+3．ステートのカスタムパラメータに任意の数値(※)を設定する
 {
-  addScore: 20
+  prohibitAttacks: 5
 }
 
-ステートスコアの参考値(StateScoreChecker.getScoreの戻り値）
+※数値はステートの評価値（AIがステートを付与する行動を取る基準値）に関連しています。
+数値が大きいほどステート攻撃を使用する頻度が高くなりますが
+一方で敵を倒せる攻撃をせずにステート付与が優先される、といったケースも起こりえます。
+
+※ステートスコアの参考値(StateScoreChecker.getScoreの戻り値）
 ・初期値 -1
 ・相手がステート無効orステートが付与されている -1を返して終了;
 
@@ -36,25 +38,35 @@ ran
 
 ■更新履歴
 2024/11/09 新規作成
+2024/11/17 ステートの指定方法を変更
 
 */
 
 (function() {
 
-// ステートid
-var STATE_ID = 0;
-
 // 該当するステートが付与されているかを判定する
-function func_checkState(unit, id)
+function func_checkState(unit)
 {
-	var state = root.getBaseData().getStateList().getDataFromId(id);
-	return StateControl.getTurnState(unit, state) !== null;
+	var i, turnState;
+	var list = unit.getTurnStateList();
+	var count = list.getCount();
+	
+	for (i = 0; i < count; i++) {
+		turnState = list.getData(i);
+		if (turnState === null) continue;
+
+		if (typeof turnState.getState().custom.prohibitAttacks === 'number') {
+			return true;
+		}
+	}
+	
+	return false;
 }
 
 // 攻撃コマンドを許可しないようにする
 var _AttackChecker_isUnitAttackable = AttackChecker.isUnitAttackable;
 AttackChecker.isUnitAttackable = function(unit) {	
-	if (func_checkState(unit, STATE_ID) === true) {
+	if (func_checkState(unit) === true) {
 		return false;
 	}
 	
@@ -64,7 +76,7 @@ AttackChecker.isUnitAttackable = function(unit) {
 // targetUnitがunitに反撃できない扱いにする
 var _AttackChecker_isCounterattack = AttackChecker.isCounterattack;
 AttackChecker.isCounterattack = function(unit, targetUnit) {
-	if (func_checkState(targetUnit, STATE_ID) === true) {
+	if (func_checkState(targetUnit) === true) {
 		return false;
 	}
 	
@@ -74,7 +86,7 @@ AttackChecker.isCounterattack = function(unit, targetUnit) {
 // AIの武器による攻撃行動を作成しないようにする
 var _CombinationCollector_Weapon__isWeaponEnabled = CombinationCollector.Weapon._isWeaponEnabled;
 CombinationCollector.Weapon._isWeaponEnabled = function(unit, item, misc) {
-	if (func_checkState(unit, STATE_ID) === true) {
+	if (func_checkState(unit) === true) {
 		return false;
 	}
 	
@@ -85,7 +97,7 @@ CombinationCollector.Weapon._isWeaponEnabled = function(unit, item, misc) {
 // 戦闘中に該当ステートが付与されたら攻撃を続行しないようにする
 var _VirtualAttackControl__isAttackStopState = VirtualAttackControl._isAttackStopState;
 VirtualAttackControl._isAttackStopState = function(virtualAttackUnit, state) {
-	if (state !== null && state.getId() === STATE_ID) {
+	if (state !== null && typeof state.custom.prohibitAttacks === 'number') {
 		return true;
 	}
 	
@@ -99,14 +111,15 @@ StateScoreChecker.getScore = function(unit, targetUnit, state) {
 	var plus;
 
 	// 本来の結果が-1（該当ステートが無効or既に付与されている）なら-1を返して終了
-	if (result === -1) return result;
-	
-	if (state === root.getBaseData().getStateList().getDataFromId(STATE_ID)) {
-		plus = typeof state.custom.addScore === 'number' ? state.custom.addScore : 5;
-		result += plus;
-		if (result < 0) result = -1;
+	if (result < 0) return result;
+
+	plus = state.custom.prohibitAttacks;
+	if (typeof plus !== 'number' || plus < 1) {
+		plus = 5;
 	}
 	
+	result += plus;
+		
 	return result;
 };
 
